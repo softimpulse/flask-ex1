99ceebaf9a32b4d8f3356c3b3e22690b559a717f
Matthias Schroeder <ms@softimpulse.de>
2017-12-03 20:12:52 +0100

    first mini example

--- hello.py
+++ hello.py
@@ -1,13 +1,6 @@
-# flask-ex1
-
-Simple Flask example, start with hello.py
-
-"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-"""
-
